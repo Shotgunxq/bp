8f0d40cfa75de5dc632e2c0a37e5e36655e4ec71
Bence <bence.bodnar1@ibm.com>
2024-02-28 11:05:11 +0100

    some missed out files

---
+++ src/app/component/Modals/login-modal/login-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-login-modal',
+  templateUrl: './login-modal.component.html',
+  styleUrl: './login-modal.component.css'
+})
+export class LoginModalComponent {
+
+}
