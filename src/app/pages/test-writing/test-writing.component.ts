e1e1bacca9801353ba7c8603345b431d18a5221b
Bence <bence.bodnar1@ibm.com>
2024-04-07 19:52:29 +0200

    fix: docker node BE build

--- src/app/pages/test-writing/test-writing.component.ts
+++ src/app/pages/test-writing/test-writing.component.ts
@@ -1,21 +1,23 @@
-import { Component } from '@angular/core';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { HttpClient } from '@angular/common/http';
+import { Component } from "@angular/core";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { HttpClient } from "@angular/common/http";
 @Component({
-  selector: 'app-test-writing',
-  templateUrl: './test-writing.component.html',
-  styleUrl: './test-writing.component.scss',
-  
+  selector: "app-test-writing",
+  templateUrl: "./test-writing.component.html",
+  styleUrl: "./test-writing.component.scss",
 })
 export class TestWritingComponent {
   constructor(private http: HttpClient) {}
 
   getData() {
-    this.http.get('http://localhost:3000/test/api').subscribe((response) => {
-      console.log('Data:', response);
-      // Process the data here
-    }, (error) => {
-      console.error('Error fetching data:', error);
-    });
+    this.http.get("http://localhost:3000/test/api").subscribe(
+      (response) => {
+        console.log("Data:", response);
+        // Process the data here
+      },
+      (error) => {
+        console.error("Error fetching data:", error);
+      },
+    );
   }
 }
