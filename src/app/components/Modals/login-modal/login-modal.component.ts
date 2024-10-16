b3631f6743e1a36bee1123cc9ac0af896a6d0d29
Bence Bodnár <bodnar.bence001@gmail.com>
2024-10-16 19:56:08 +0100

    added password visibility button need fix css

--- src/app/components/Modals/login-modal/login-modal.component.ts
+++ src/app/components/Modals/login-modal/login-modal.component.ts
@@ -1,17 +1,27 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';  // Import Router
-
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
 @Component({
   selector: 'app-login-modal',
   templateUrl: './login-modal.component.html',
   styleUrls: ['./login-modal.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: false,
 })
 export class LoginModalComponent {
   username: string = '';  // Bound to the username input
   password: string = '';  // Bound to the password input
   errorMessage: string = '';  // To show error messages if login fails
-
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
   constructor(private http: HttpClient, private router: Router) {}  // Inject Router
 
   //TODO: include apiService
