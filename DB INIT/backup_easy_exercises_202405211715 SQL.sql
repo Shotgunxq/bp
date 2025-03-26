874d22ddc9eddc1d95fe1997321b62f82c63270f
Bence Bodnár <bodnar.bence001@gmail.com>
2025-03-26 17:20:51 +0100

    fix: delete dialog, new exercise images

--- DB INIT/backup_easy_exercises_202405211715 SQL.sql
+++ DB INIT/backup_easy_exercises_202405211715 SQL.sql
@@ -1,16 +1,16 @@
-INSERT INTO exercises (exercise_id,difficulty,description,image,points,answer) VALUES
+INSERT INTO exercises (exercise_id,difficulty,description,points,answer) VALUES
 	 (1,'easy','Ak sú náhodné udalostia P je pravdepodobnosť, potom platí
 P(A) = P( A ∩ B) + P(A ∩ BC)
-',NULL,NULL,'Pravda'),
+',NULL,'Pravda'),
 	 (2,'easy','Ak A, B sú náhodné udalostia a P je pravdepodobnosť, potom platí
-P ( A ∩ B ) ≤ P(A) + P(B) – P(A ∪ B) ',NULL,NULL,'Pravda'),
-	 (3,'easy','V jednej krabici je 10 modrých a 5 červených balónov. V druhej krabici je osem bielych a 12 modrých balónov. Náhodne si zvolíme jednu krabicu a vyberieme z neho 1 balón. Aká je pravdepodobnosť, že z nej vytiahnutý balón bude červený?',NULL,NULL,'0,16'),
-	 (4,'easy','V jednej krabici je 10 modrých a 5 červených balónov. V druhej krabici je osem bielych a 12 modrých balónov. Náhodne si zvolíme jednu krabicu a vyberieme z neho I balón. Aká je pravdepodobnosť, že z nej vytiahnutý balón bude biely?',NULL,NULL,'0,2'),
+P ( A ∩ B ) ≤ P(A) + P(B) – P(A ∪ B) ',NULL,'Pravda'),
+	 (3,'easy','V jednej krabici je 10 modrých a 5 červených balónov. V druhej krabici je osem bielych a 12 modrých balónov. Náhodne si zvolíme jednu krabicu a vyberieme z neho 1 balón. Aká je pravdepodobnosť, že z nej vytiahnutý balón bude červený?',NULL,'0,16'),
+	 (4,'easy','V jednej krabici je 10 modrých a 5 červených balónov. V druhej krabici je osem bielych a 12 modrých balónov. Náhodne si zvolíme jednu krabicu a vyberieme z neho I balón. Aká je pravdepodobnosť, že z nej vytiahnutý balón bude biely?',NULL,'0,2'),
 	 (5,'easy','Vieme že prístupový kód sa skladá, z piatich rôznych znakov z množiny { 1, 2, 3, 4, 5, 6}.
 Aká je pravdepodobnosť, že na prvý krát uhádneme prístupový kód?
-Výsledok vyjadrite na 4 desatinné miesta.',NULL,NULL,'0,0013888'),
+Výsledok vyjadrite na 4 desatinné miesta.',NULL,'0,0013888'),
 	 (6,'easy','V lietadle je 20% cestujúcich zo SR- Je známe, Že 60% obyvaterov SR pije po obede pivo, kým obyvatelia iných štátov vypijú pivo po obede len v 20%.
-Cestujúci po obede si vypýta pivo. S akou pravdepodobnosťou je to občan SR?',NULL,NULL,'0,42857'),
+Cestujúci po obede si vypýta pivo. S akou pravdepodobnosťou je to občan SR?',NULL,'0,42857'),
 	 (7,'easy','V krabici s 20-timi fixkami sa nachádzajú 3 vadné. Vyberieme S. Náhodná premenná X predstavuje počet vadných fixiek.
-Ako sa rozdelenie pravdepodobnosti náhodnej premennej X?',NULL,NULL,'Hypergeometrické'),
-	 (8,'easy','Náhodná premenná X ~ Bi(n; p) a vieme, že E(X) = 9 a D(X) = 6,3. Vypocitajte disperziu n.p. Y = 2X -1.',NULL,NULL,'25,2');
+Ako sa rozdelenie pravdepodobnosti náhodnej premennej X?',NULL,'Hypergeometrické'),
+	 (8,'easy','Náhodná premenná X ~ Bi(n; p) a vieme, že E(X) = 9 a D(X) = 6,3. Vypocitajte disperziu n.p. Y = 2X -1.',NULL,'25,2');
